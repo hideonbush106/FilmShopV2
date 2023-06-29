a22feadc5d34d3de709e6f8a54b851eaf8a1233c
Nguyen Hoai Phong <andyhoaiphong2003@gmail.com>
2023-06-29 17:10:24 +0700

    Config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import markdownRawPlugin from 'vite-raw-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    markdownRawPlugin({
-      fileRegex: /\.md$/
-    })
-  ],
-  server: {
-    port: 3000
-  },
-  css: {
-    devSourcemap: true
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: env.APP_ENV
+    },
+    plugins: [
+      react(),
+      markdownRawPlugin({
+        fileRegex: /\.md$/
+      })
+    ],
+    server: {
+      port: 3000
+    },
+    css: {
+      devSourcemap: true
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
     }
   }
 })
